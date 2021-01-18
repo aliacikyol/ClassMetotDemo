c49c09d00dff7ce3c5ff0e86fc0ba6d275cc54ef
ALİ AÇIKYOL <aliacikyol6301@hotmail.com>
2021-01-18 19:33:39 +0300

    Bankacılık Takip Sistemi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.AdSoyad + " isimli " + musteri.Iban + " İban numaralı müşteri bankacılık sistemine eklenmiştir ");
+
+
+        }
+        public void Sil(Musteri musteri)
+        {
+
+            Console.WriteLine(musteri.AdSoyad + " isimli " + musteri.Iban + " İban numaralı müşteri bankacılık sisteminden silinmiştir. ");
+
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.AdSoyad + " - " + musteri.Iban);
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,55 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+            musteri1.AdSoyad = "Sultan Çelebioğulları";
+            musteri1.Iban = "TR340000154214000124578965";
+            
+
+            Musteri musteri2 = new Musteri();
+            musteri2.AdSoyad = "Nemide Çelebioğulları";
+            musteri2.Iban = "TR340000123456789456123045";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.AdSoyad = "Mustafa Nuri Çelebioğulları";
+            musteri3.Iban = "TR34000123456789654258369";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2,musteri3 };
+
+            Console.WriteLine("ABC Bank Müşteri Takip Sistemi");
+            Console.WriteLine("---------------------------------");
+
+            foreach (Musteri musteri in musteriler)
+            {
+                
+                Console.WriteLine(musteri.AdSoyad);
+                
+                Console.WriteLine(musteri.Iban);
+                Console.WriteLine("------------------");
+
+            }
+            
+            
+            
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            Console.WriteLine("--------------------");
+
+            musteriManager.Sil(musteri2);
+
+            Console.WriteLine("--------------------");
+
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+
+
+
+
+
         }
     }
 }
